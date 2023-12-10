37798c8f789d4d81b67066baaabaf8a38cd56b1b
Rizqi Ari Putra <riezq.25@gmail.com>
2023-12-10 13:34:37 +0700

    [FEAT] Add Transaction model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Cart::class);
     }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
