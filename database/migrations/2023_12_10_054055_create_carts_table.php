99df9b2183c6fa2e3d97e9d0e5b6797ba9b44c2a
Rizqi Ari Putra <riezq.25@gmail.com>
2023-12-10 12:45:56 +0700

    [FIX] Add 'jumlah' column to carts table

--- database/migrations/2023_12_10_054055_create_carts_table.php
+++ database/migrations/2023_12_10_054055_create_carts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('jumlah')->default(1);
             $table->timestamps();
         });
     }
