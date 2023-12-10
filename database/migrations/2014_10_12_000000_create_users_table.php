9156af65bf459d0bb3700f0751c6e9f38f9c33f2
Rizqi Ari Putra <riezq.25@gmail.com>
2023-12-10 10:59:15 +0700

    [FIX] Alter user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum(
+                'role',
+                [
+                    'penjual',
+                    'pembeli',
+                ]
+            )->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
