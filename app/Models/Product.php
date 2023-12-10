f419c57a84527debc26e372aef3cb5512c74729c
Rizqi Ari Putra <riezq.25@gmail.com>
2023-12-10 12:42:58 +0700

    [FEAT] Add Cart model and relationships
    This commit adds the Cart model and its relationships with User and Product models.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,4 +26,9 @@ class Product extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
